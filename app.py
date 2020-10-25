4b11bc16acdd527ded9921f83191b481dcb23ce6
Tracey Phuong Ha <thao.ph.ha@gmail.com>
2020-10-25 13:37:27 +1100

    Create a draft app

--- app.py
+++ app.py
@@ -1,38 +1,28 @@
 # import necessary libraries
+import pandas as pd
 import os
+import simplejson
 from sqlalchemy.sql import select, column, text
 from sqlalchemy.sql.expression import func
-from flask import (
-    Flask,
-    render_template,
-    jsonify,
-    request,
-    redirect)
+from flask import (Flask, render_template, jsonify, request, redirect)
 from models import create_classes
-import simplejson
 from flask_sqlalchemy import SQLAlchemy
+from joblib import load
+from model.train import load_model, load_data, split_data, test_model
 
-"""
-Let's setup our Flask application.
-"""
+# Let's setup our Flask application.
 app = Flask(__name__)
 
 """
-Next database connection setup.
-
-Let's look for an environment variable 'DATABASE_URL'. If there isn't one,
-we'll use a connection to a sqlite database.
+Database connection setup: Let's look for an environment variable 'DATABASE_URL'.
+If there isn't one, we'll use a connection to a sqlite database.
 """
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL', '') or "sqlite:///db.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
 
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-"""
-Using the `flask_sqlalchemy` library we'll creating our
-variable `db` that is the connection to our database
-"""
+# Using the `flask_sqlalchemy` library we'll create our variable `db` that is the connection to our database
 db = SQLAlchemy(app)
 
 """
@@ -68,6 +58,93 @@ def all():
     ).all()
 
     return query_results_to_dicts(results)
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    Display the main webpage where users can enter theirs details
+    which we will then pass to the prediction endpoint
+    """
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json    
+
+    col_order = [
+        "age","sibsp","parch","fare","is_female",
+        "embarked_c","embarked_q","pclass_1","pclass_2"
+        ]
+
+    rename_cols = {
+         "age": "age", 
+         "numberOfSiblings": "sibsp", 
+         "numberOfParents": "parch", 
+         "fare": "fare", 
+        #"gender", 
+        #"passengerClass", 
+        #"portOfEmbarkment": ""
+    }
+
+    # convert gender to is_female
+    if (data["gender"] == "female"):
+        data["is_female"] = 1
+    else:
+        data["is_female"] = 0
+    
+    del data["gender"]
+
+    # convert passengerClass
+    if (data["passengerClass"] == 1):
+        data["pclass_1"] = 1
+        data["pclass_2"] = 0
+    elif (data["passengerClass"] == 2):
+        data["pclass_1"] = 0
+        data["pclass_2"] = 1
+    elif (data["passengerClass"] == 3):
+        data["pclass_1"] = 0
+        data["pclass_2"] = 0
+
+    del data["passengerClass"]
+    
+    # convert portOfEmbarkment
+    if (data["portOfEmbarkment"] == "S"):
+        data["embarked_c"] = 0
+        data["embarked_q"] = 0
+    elif (data["portOfEmbarkment"] == "C"):
+        data["embarked_c"] = 1
+        data["embarked_q"] = 0        
+    elif (data["portOfEmbarkment"] == "Q"):
+        data["embarked_c"] = 0
+        data["embarked_q"] = 1
+    
+    del data["portOfEmbarkment"]
+    
+    # create dataframe from received data
+    # rename columns and sort as per the 
+    # order columns were trained on
+    try:
+        df = pd.DataFrame([data]).rename(columns=rename_cols)[col_order]
+    except Exception as e:
+        print("Error Parsing Input Data")
+        print(e)
+        return "Error"
+
+    X = df.values
+
+    model = load_model()
+
+    # convert nparray to list so we can
+    # serialise as json
+    result = model.predict(X).tolist()    
+
+    return jsonify({"result": result})
 
 # def get_selected_race():
 #     """

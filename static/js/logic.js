1f79a2c3176b222151aa3f4e4e57632ea7508d00
Tracey Phuong Ha <thao.ph.ha@gmail.com>
2020-11-07 07:09:49 +1100

    Add giveFeedback function

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,3 @@
-// // Perform an api call to the cuisine_ingredients data
-// d3.json("/api/all").then(function(data) {
-// 	console.log(data)
-// });
-
 // Event: add ingredients' text to find cuisine
 d3.select("#findCuisine").on("click", (event) => findCuisine(event));
 
@@ -11,12 +6,10 @@ function findCuisine(event) {
 
 	let ingredients = d3.select("#ingredients").node().value;
     
-    if (ingredients==="") {
+    if (ingredients.trim() === "") {
         d3.select("#alertOutcome").text("Please add some ingredients.");
     } else {
-        let data = {
-            "ingredients": ingredients
-        };
+        let data = {"ingredients": ingredients};
     
         d3.json(
             "/predict", {
@@ -32,13 +25,11 @@ function findCuisine(event) {
     }
 }
 
-
 function showResult(result) {
     var outcome = result["result"][0];
-    console.log(outcome);
     d3.select("#alertOutcome").html(
 		`<span>
-		Hei, are you craving for some <b>${outcome}</b> food?
+		Are you craving for some <b>${outcome}</b> food?
 		<i class="fas fa-pepper-hot"></i>
 		<i class="fas fa-pizza-slice"></i>
         </span>
@@ -46,36 +37,43 @@ function showResult(result) {
 	);
 }
 
+// Clear text in textarea
 d3.select("#clear").on("click", (event) => clear(event));
 
 function clear() {
 	document.getElementById("ingredients").value="";
 }
 
+// Event: user gives feedback about model's predictions
+d3.select("#giveFeedback").on("click", (event) => giveFeedback(event));
 
-// d3.select("#give_feedback").on("click", (event) => findCuisine(event));
+function giveFeedback(event) {
+    d3.event.preventDefault();
+    console.log("Giving Feedback");
 
-// function findCuisine(event) {
-//     d3.event.preventDefault();
-//     console.log("Checking Cuisine");
+    let chosenCuisine = d3.select("#chosenCuisine").node().value;
+    let enteredCuisine = d3.select("#enteredCuisine").node().value;
+	let recipeName = d3.select("#recipeName").node().value;
+	let recipeLink = d3.select("#recipeLink").node().value;
 
-// 	let ingredients = d3.select("#ingredients").node().value;
-	
-// 	let data = {
-// 		"ingredients": ingredients
-// 	};
+	let feedback = {
+        "chosenCuisine": chosenCuisine,
+        "enteredCuisine": enteredCuisine,
+        "recipeName": recipeName,
+        "recipeLink": recipeLink,
+	};
 
-//     console.log(data);
+    console.log(feedback);
 
-//     d3.json(
-//         "/predict", {
-//             method: "POST",
-//             body: JSON.stringify(data),
-//             headers: {
-//                 "Content-type": "application/json; charset=UTF-8"
-//             }
-//         }
-//     ).then(
-//         (result) => showResult(result)
-//     );
-// }
+    d3.json(
+        "/feedback", {
+            method: "POST",
+            body: JSON.stringify(feedback),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        }
+    ).then(
+        d3.select("#thankyou").text("Thanks for your feedback!")
+    );
+}

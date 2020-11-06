90ccffe5843dc14e9b8115cd718af30a51f836e0
Tracey Phuong Ha <thao.ph.ha@gmail.com>
2020-11-06 15:21:48 +1100

    Change fonts to Nora Sans

--- static/js/logic.js
+++ static/js/logic.js
@@ -7,15 +7,12 @@ d3.select("#findCuisine").on("click", (event) => findCuisine(event));
 
 function findCuisine(event) {
     d3.event.preventDefault();
-    console.log("Checking Cuisine");
-
+    
 	let ingredients = d3.select("#ingredients").node().value;
 	
 	let data = {
 		"ingredients": ingredients
 	};
-
-    console.log(data);
 
     d3.json(
         "/predict", {
@@ -32,10 +29,7 @@ function findCuisine(event) {
 
 
 function showResult(result) {
-    console.log("showResult");
-    console.log(result);
 	var outcome = result["result"][0];
-	console.log(outcome);
     d3.select("#alertOutcome").html(
 		`<span>
 		Hei, are you craving for some <b>${outcome}</b> food?
@@ -51,3 +45,31 @@ d3.select("#clear").on("click", (event) => clear(event));
 function clear() {
 	document.getElementById("ingredients").reset();
 }
+
+
+// d3.select("#give_feedback").on("click", (event) => findCuisine(event));
+
+// function findCuisine(event) {
+//     d3.event.preventDefault();
+//     console.log("Checking Cuisine");
+
+// 	let ingredients = d3.select("#ingredients").node().value;
+	
+// 	let data = {
+// 		"ingredients": ingredients
+// 	};
+
+//     console.log(data);
+
+//     d3.json(
+//         "/predict", {
+//             method: "POST",
+//             body: JSON.stringify(data),
+//             headers: {
+//                 "Content-type": "application/json; charset=UTF-8"
+//             }
+//         }
+//     ).then(
+//         (result) => showResult(result)
+//     );
+// }
